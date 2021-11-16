0e383f0190288944e6bfe881e5121a16d62e4bc8
Cristóbal Nettle <cristobal.nettle@gmail.com>
2021-11-16 14:12:48 -0300

    restores -M --max-scholar option

--- searchCitations_Options.py
+++ searchCitations_Options.py
@@ -17,6 +17,8 @@ parser.add_option("-l", "--lastTry", dest="lastTry", default=None,
                   help="(Optional) If INPUT_CSV is given, another identified named as LAST_TRY is appended to a title if the title alone is not enough for finding the article. If there is no identifier in the CSV file or ARTICLE_TITLE is given, LAST_TRY is appended to the title.", metavar="LAST_TRY")
 parser.add_option("-m", "--match-percent", dest="matcher", type="float", default=1,
                   help="When titles are compared, a matching percentage can be use, to avoid hard exact comparitons. [default: %default]", metavar="MATCH")
+parser.add_option("-M", "--max-scholar", dest="mscholar", type="int", default=5,
+                  help="Sets the maximum number of gscholar for comparing with the searched title. [default: %default]", metavar="MAXSCH")
 parser.add_option("-n", "--not-found", dest="saveNotFound", action="store_true", default=False,
                   help="Flag that enables the storing a not-found.csv file containing a resummee of the articles already searched for but that weren't found. [default: %default]", metavar="NFOUND")
 parser.add_option("--notify", dest="notify", action="store_true", default=False,
